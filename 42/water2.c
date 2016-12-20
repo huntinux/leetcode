816c4fd7c4abf6cb02f006b18043b62c592e05cf
jinger <Huntinux@gmail.com>
2016-12-20 19:05:36 +0800

    Update water2.c

--- 42/water2.c
+++ 42/water2.c
@@ -1,4 +1,42 @@
 /*
+使用两个index，分别指向数组的两边，记为left和right。每次比较A[left]和A[rgith],记较小者为min，那么从较小的一边开始，
+例如此时A[left]<A[rgith],所以min=A[left], 那么如果A[left+1] < A[left], 那么A[left+1]上面可以存的水量是： A[left+1] - A[left]。
+即：A[left+1] - min
+一直到A[left+N] < min不满足条件为止
+*/
+int trap(const int arr[], size_t n)
+{
+    if(n < 3) return 0;
+
+    int result = 0;
+    int left = 0, right = n-1;
+    
+
+    while(left < right) {
+        if(arr[left] < arr[right]) {
+            int min = arr[left];
+            left++;
+            while(left < right && arr[left] < min) {
+                result += min - arr[left];
+                left++;
+            }
+        } else {
+            int min = arr[right];
+            right--;
+            while(left < right && arr[right] < min) {
+                result += min - arr[right];
+                right--;
+            }
+        }
+    }
+    return result;
+}
+
+
+
+
+
+/*
 WRONG SOLUTION!  SEE: http://shmilyaw-hotmail-com.iteye.com/blog/2289919
 
 时间：O(n) 空间：O(1)
@@ -10,6 +48,7 @@ WRONG SOLUTION!  SEE: http://shmilyaw-hotmail-com.iteye.com/blog/2289919
 
 初始状态， right_max = A[n-1], left_max = A[0]
 */
+
 
 
 int trap(int arr[], size_t n)
