03376fe00d7ff7a6c43ba6db56d69de2cdba6d3d
huntinux <huntinux@gmail.com>
2021-05-27 22:28:08 +0800

    sort: quick_sort update

--- src/sort/common/quick_sort/quick_sort.cc
+++ src/sort/common/quick_sort/quick_sort.cc
@@ -5,27 +5,27 @@ static int partition(std::vector<int>& vec, int low, int high) {
   int pivot_val = vec[pivot];
   while (low < high) {
     // 在右边找到第一个小于pivot_val的元素，与pivot交换位置(这里没必要每次都交换
-    // 左后得到pivot最终位置再赋值即可
+    // 最后得到pivot最终位置再赋值即可
     while (low < high && vec[high] >= pivot_val) --high;
     if (low < high) {
-      vec[pivot] = vec[high];
-      //++low; // 减少没必要的比较
+      vec[low++] = vec[high]; // low指向的位置即为pivot的原位置，即为high的目标
+                              // 位置，且下次从low后面开始比较即可，因此low++
+                              // high的位置变为pivot的位置, 即为空缺位置
     }
     while (low < high && vec[low] <= pivot_val) ++low;
     if (low < high) {
-      vec[pivot] = vec[low];
-      //--high; // 减少没必要的比较
+      vec[high--] = vec[low];
     }
   }
-  pivot = low; // 最后low与high相遇的位置几位pivot的最终位置
-  vec[pivot] = pivot_val; // 回复pivot位置的值
+  pivot = low;  // 最后low与high相遇的位置几位pivot的最终位置
+  vec[pivot] = pivot_val;  // 回复pivot位置的值
   return pivot;
 }
 
 void quick_sort_helper(std::vector<int>& vec, int low, int high) {
   if (low < high) {
     int pivot = partition(vec, low, high);  // 确定了一个元素的位置后
-    quick_sort_helper(vec, low, pivot - 1); // 分别对左右两部分进行同样的操作
+    quick_sort_helper(vec, low, pivot - 1);  // 分别对左右两部分进行同样的操作
     quick_sort_helper(vec, pivot + 1, high);
   }
 }
