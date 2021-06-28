a28334fb119506fc2280782facfb2afa3abea30e
jinger <huntinux@gmail.com>
2021-06-28 11:38:16 +0800

    83. Remove Duplicates from Sorted List

--- src/linear/list/remove_duplicates_from_sorted_list/solution.cc
+++ src/linear/list/remove_duplicates_from_sorted_list/solution.cc
@@ -17,7 +17,7 @@
 
 Node* Solution::DeleteDuplicates(Node* head) {
   if (head == nullptr) return nullptr;
-  for (Node* prev = head, *cur = head->next; cur; cur = prev->next) {
+  for (Node *prev = head, *cur = head->next; cur; cur = prev->next) {
     if (prev->value == cur->value) {
       prev->next = cur->next;
       delete cur;
@@ -26,4 +26,22 @@ Node* Solution::DeleteDuplicates(Node* head) {
     }
   }
   return head;
+}
+
+// 利用dummy头节点的解法
+ListNode* deleteDuplicates(ListNode* head) {
+  if (head == nullptr) return nullptr;
+  ListNode dummy(head->val - 1, head);
+  ListNode *curr = head, *prev = &dummy;
+  while (curr) {
+    if (curr->val == prev->val) {
+      prev->next = curr->next;
+      delete curr;
+      curr = prev->next;
+    } else {
+      prev = curr;
+      curr = curr->next;
+    }
+  }
+  return dummy.next;
 }
