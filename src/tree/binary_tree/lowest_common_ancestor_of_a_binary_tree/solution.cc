190fe9f1bdd1e4d482e73f118af4ff22ff1c3ee4
jinger <huntinux@gmail.com>
2021-07-16 13:59:28 +0800

    236. Lowest Common Ancestor of a Binary Tree

--- src/tree/binary_tree/lowest_common_ancestor_of_a_binary_tree/solution.cc
+++ src/tree/binary_tree/lowest_common_ancestor_of_a_binary_tree/solution.cc
@@ -1,6 +1,37 @@
 #include "solution.h"
 
 // 递归写法
+//
+//     p
+//    /
+//   .
+//  /
+// q
+//
+//     q
+//    /
+//   .
+//  /
+// p
+//
+//    root
+//    / \
+//   p   q
+//
+//    root
+//    / \
+//   a   b
+//  /   /
+// p   q
+//
+// lca(root, p, q)->root
+//  lca(a, p, q)->p
+//   lca(p, p, q)->p
+//   lca(nullptr, p, q)->nullptr
+//  lca(b, p, q)->q
+//   lca(q, p, q)->q
+//   lca(nullptr, p, q)->nullptr
+//
 TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
   if (!root || p == root || q == root) return root;
   TreeNode* left = lowestCommonAncestor(root->left, p, q);
@@ -9,3 +40,36 @@ TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
   if (left != nullptr && right != nullptr) return root;
   return left == nullptr ? right : left;
 }
+
+// 存储父节点
+// 我们可以用哈希表存储所有节点的父节点，然后我们就可以利用节点的父节点信息从 p
+// 结点开始不断往上跳，并记录已经访问过的节点，再从 q
+// 节点开始不断往上跳，如果碰到已经访问过的节点，那么这个节点就是我们要找的最近公共祖先。
+class Solution {
+ public:
+  unordered_map<int, TreeNode*> fa;
+  unordered_map<int, bool> vis;
+  void dfs(TreeNode* root) {
+    if (root->left != nullptr) {
+      fa[root->left->val] = root;
+      dfs(root->left);
+    }
+    if (root->right != nullptr) {
+      fa[root->right->val] = root;
+      dfs(root->right);
+    }
+  }
+  TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    fa[root->val] = nullptr;
+    dfs(root);
+    while (p != nullptr) {
+      vis[p->val] = true;
+      p = fa[p->val];
+    }
+    while (q != nullptr) {
+      if (vis[q->val]) return q;
+      q = fa[q->val];
+    }
+    return nullptr;
+  }
+};
--- src/tree/binary_tree/lowest_common_ancestor_of_a_binary_tree/solution.h
+++ src/tree/binary_tree/lowest_common_ancestor_of_a_binary_tree/solution.h
@@ -18,5 +18,8 @@ using std::vector;
  */
 class Solution {
  public:
+  // 所有 Node.val 互不相同 。
+  // p != q
+  // p 和 q 均存在于给定的二叉树中。
   TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
 };
